a58c2b6528e5c9c9794c866748adb81a6577f59f
Marcus Hurney <marcushurney@gmail.com>
2016-10-26 17:02:11 -0400

    Fixes footer

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  // add fadeInDown animation
+   $('.js-wp-1').waypoint(function(direction) {
+     $('.js-wp-1').addClass('animated fadeIn');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add fadeInLeft animation
+   $('.js-wp-2').waypoint(function(direction) {
+     $('.js-wp-2').addClass('animated fadeIn');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add fadeInRight animation
+   $('.js-wp-3').waypoint(function(direction) {
+     $('.js-wp-3').addClass('animated fadeIn');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add fadeInUp animation
+   $('.js-wp-4').waypoint(function(direction) {
+     $('.js-wp-4').addClass('animated fadeIn');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add flipInX animation
+   $('.js-wp-5').waypoint(function(direction) {
+     $('.js-wp-5').addClass('animated flipInX');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add flipInX animation
+   $('.js-wp-6').waypoint(function(direction) {
+     $('.js-wp-6').addClass('animated flipInX');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add flipInX animation
+   $('.js-wp-7').waypoint(function(direction) {
+     $('.js-wp-7').addClass('animated flipInX');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add flipInX animation
+   $('.js-wp-8').waypoint(function(direction) {
+     $('.js-wp-8').addClass('animated slideInUp');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add flipInX animation
+   $('.js-wp-9').waypoint(function(direction) {
+     $('.js-wp-9').addClass('animated flipInX');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+
+   // add flipInX animation
+   $('.js-wp-10').waypoint(function(direction) {
+     $('.js-wp-10').addClass('animated flipInX');
+   }, {
+     // offset determines when the animation should start
+     offset: '50%'
+   });
+});
